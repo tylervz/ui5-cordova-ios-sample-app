c9e0396b50355a91129a1f0e2da7a67042f8da33
Tyler VanZanten <vanzanten.tyler@gmail.com>
2021-02-18 16:31:57 -0600

    Add some simple controls on the HomePage

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -1,15 +1,67 @@
 sap.ui.define([
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment",
 	"sap/ui/core/mvc/Controller",
 	"../model/formatter"
-], function(Controller, formatter) {
+], function(MessageBox, MessageToast, Fragment, Controller, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
 
 		formatter: formatter,
 
-		onInit: function () {
+		onInit: function() {
 
+		},
+
+		handleShowMessageButton: function() {
+			var title = "Hello";
+			var message = "MessageBox is working fine.";
+			MessageBox.show(
+				message, {
+					title: title,
+					actions: [MessageBox.Action.OK]
+				}
+			);
+		},
+
+		handleOpenActionSheetButton: function(oEvent) {
+			var oButton = oEvent.getSource();
+			// Create action sheet only once
+			if (!this.byId("homePageActions")) {
+				var that = this;
+				var oView = this.getView();
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.basicTemplate.fragments.HomePageActions",
+					controller: this
+				}).then(function (oActionSheet) {
+					oView.addDependent(oActionSheet);
+					oActionSheet.openBy(oButton);
+				});
+			} else {
+				var actionSheet = this.byId("homePageActions");
+				actionSheet.openBy(oButton);
+			}
+		},
+
+		openAlert: function() {
+			var title = "Alert";
+			var message = "MessageBox is still working fine.";
+			MessageBox.show(
+				message, {
+					icon: MessageBox.Icon.ERROR,
+					title: title,
+					actions: [MessageBox.Action.OK]
+				}
+			);
+		},
+
+		openToast: function() {
+			var message = "This is a toast :)";
+			MessageToast.show(message);
 		}
 	});
 });
